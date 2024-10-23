9a99e2d231f1c2fd6193604925eaa5b11039b01a
broozi <behshad.nowroozi@gmail.com>
2024-10-22 22:14:41 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Behshad Nowroozi on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
